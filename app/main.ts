1b8d04557e274bf37230cef33f85a07d2bd28a7e
AmitThakkar <vigildbest@gmail.com>
2016-07-15 23:41:26 +0530

    Adding frontend hello world app.

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by amitthakkar on 15/07/16.
+ */
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+bootstrap(<any>AppComponent);
