d45f18340d54571aa1677e054b1f7f27e237f97b
AmitThakkar <vigildbest@gmail.com>
2016-07-15 19:50:37 +0530

    Adding album list api.

--- server/components/artist/artist.controller.js
+++ server/components/artist/artist.controller.js
@@ -8,15 +8,16 @@ const HTTP_STATUS = require('http-status');
 
 // Constants
 const API_URL = 'https://itunes.apple.com/';
-const RETRIEVE_ARTIST_URL = API_URL + 'search?term=';
+const RETRIEVE_ARTIST_URL = API_URL + 'search?term={artistName}';
+const RETRIEVE_ALBUM_LIST = API_URL + 'lookup?id={artistId}&entity=album';
 
 // API Handler
-exports.listAlbum = (request, response) => {
+exports.listArtist = (request, response) => {
     let artistName = request.query.artistName;
     if (!artistName) {
         return response.status(HTTP_STATUS.UNPROCESSABLE_ENTITY).json({message: "Please provide artist Name"});
     }
-    REQUEST(RETRIEVE_ARTIST_URL + artistName, function (error, artistResponse, body) {
+    REQUEST(RETRIEVE_ARTIST_URL.replace('{artistName}', artistName), function (error, artistResponse) {
         if (error) {
             return response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: error});
         } else if (artistResponse.statusCode == HTTP_STATUS.OK) {
@@ -38,5 +39,34 @@ exports.listAlbum = (request, response) => {
             }
         }
     })
-
+};
+exports.listAlbum = (request, response) => {
+    let artistId = request.query.artistId;
+    if (!artistId) {
+        return response.status(HTTP_STATUS.UNPROCESSABLE_ENTITY).json({message: "Please provide artist Id"});
+    }
+    REQUEST(RETRIEVE_ALBUM_LIST.replace('{artistId}', artistId), function (error, albumResponse) {
+        if (error) {
+            return response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: error});
+        } else if (albumResponse.statusCode == HTTP_STATUS.OK) {
+            let result = JSON.parse(albumResponse.body);
+            if (result && result.resultCount > 0) {
+                let albumArray = [];
+                result.results.forEach((album, index) => {
+                    if (index === 0) {
+                        return;
+                    }
+                    albumArray.push({
+                        artistName: album.artistName,
+                        artworkUrl100: album.artworkUrl100,
+                        collectionId: album.collectionId,
+                        collectionName: album.collectionName
+                    });
+                });
+                response.status(HTTP_STATUS.OK).json(albumArray);
+            } else {
+                return response.status(HTTP_STATUS.NOT_FOUND).json({message: "No Artist Fount with name " + artistId});
+            }
+        }
+    })
 };
--- server/components/artist/artist.main.js
+++ server/components/artist/artist.main.js
@@ -5,5 +5,6 @@
 // Route Mappings
 module.exports = (app) => {
     const ArtistController = require('./artist.controller');
+    app.get('/api/artistList', ArtistController.listArtist);
     app.get('/api/albumList', ArtistController.listAlbum);
 };
