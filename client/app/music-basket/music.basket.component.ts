29fc08fc6f12a18bbb15d13b5ebf43e535505e70
AmitThakkar <vigildbest@gmail.com>
2016-07-16 15:30:17 +0530

    Adding autoCompolete click event.

--- client/app/music-basket/music.basket.component.ts
+++ client/app/music-basket/music.basket.component.ts
@@ -2,10 +2,44 @@
  * Created by amitthakkar on 15/07/16.
  */
 import {Component} from '@angular/core';
+import {MusicBasketService} from "./music.basket.service";
+
 @Component({
     selector: 'my-app',
     moduleId: module.id,
-    templateUrl: './music.basket.html'
+    templateUrl: './music.basket.html',
+    providers: [MusicBasketService]
 })
 export class MusicBasketComponent {
+    private requestWaitTime:number = 200;
+    private artistName:string = '';
+    private refreshTimer:any = undefined;
+    private searchInProgress:any = undefined;
+    private nextSearchRequired:any = undefined;
+
+    constructor(private _musicBasketService:MusicBasketService) {
+    }
+
+    public keyup(event:any) {
+        this.artistName = event.target.value;
+        if (this.refreshTimer) {
+            clearTimeout(this.refreshTimer);
+        }
+        this.refreshTimer = setTimeout(() => {
+            if (!this.searchInProgress) {
+                this.listArtist();
+            } else {
+                this.nextSearchRequired = true;
+            }
+        }, this.requestWaitTime);
+    }
+
+    public listArtist() {
+        this.searchInProgress = true;
+        this._musicBasketService.listArtist(this.artistName)
+        this.searchInProgress = false;
+        if (this.nextSearchRequired) {
+            this.listArtist();
+        }
+    }
 }
---
+++ client/app/music-basket/music.basket.service.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by amitthakkar on 16/07/16.
+ */
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class MusicBasketService {
+    public listArtist(artistName:string) {
+
+    }
+}
